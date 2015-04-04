704d05536da5009a7422766503fb8d6f35860161
Ben Wang <benfounder@gmail.com>
2015-04-04 19:39:57 +0800

    Fix compile error

--- credis-test.c
+++ credis-test.c
@@ -126,7 +126,7 @@ int main(int argc, char **argv) {
   printf("> connected_clients: %d\n", info.connected_clients);
   printf("> connected_slaves: %d\n", info.connected_slaves);
   printf("> blocked_clients: %d\n", info.blocked_clients);
-  printf("> used_memory: %zu\n", info.used_memory);
+  printf("> used_memory: %lu\n", info.used_memory);
   printf("> used_memory_human: %s\n", info.used_memory_human);
   printf("> changes_since_last_save: %lld\n", info.changes_since_last_save);
   printf("> bgsave_in_progress: %d\n", info.bgsave_in_progress);
@@ -135,8 +135,8 @@ int main(int argc, char **argv) {
   printf("> total_connections_received: %lld\n", info.total_connections_received);
   printf("> total_commands_processed: %lld\n", info.total_commands_processed);
   printf("> expired_keys: %lld\n", info.expired_keys);
-  printf("> hash_max_zipmap_entries: %zu\n", info.hash_max_zipmap_entries);
-  printf("> hash_max_zipmap_value: %zu\n", info.hash_max_zipmap_value);
+  printf("> hash_max_zipmap_entries: %lu\n", info.hash_max_zipmap_entries);
+  printf("> hash_max_zipmap_value: %lu\n", info.hash_max_zipmap_value);
   printf("> pubsub_channels: %ld\n", info.pubsub_channels);
   printf("> pubsub_patterns: %u\n", info.pubsub_patterns);
   printf("> vm_enabled: %d\n", info.vm_enabled);
